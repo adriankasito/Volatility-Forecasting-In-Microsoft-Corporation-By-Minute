2fea2adbf69cf950c2528f1a1c25537539a579c0
Mbirungi <mbirungi@keiphoneglobal.com>
2024-01-25 12:03:00 +0300

    refactored code

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-"""This module extracts information from your `.env` file so that
-you can use your AplhaVantage API key in other parts of the application.
-"""
+# This module extracts information from your `.env` file so that
+# you can use your AlphaVantage API key in other parts of the application.
 
 # The os library allows you to communicate with a computer's
 # operating system: https://docs.python.org/3/library/os.html
@@ -18,8 +17,14 @@ def return_full_path(filename: str = ".env") -> str:
     return full_path
 
 
+def get_model_directory() -> str:
+    """Constructs the model directory path relative to the script location."""
+    script_directory = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(script_directory, "")
+
+
 class Settings(BaseSettings):
-    """Uses pydantic to define settings for project."""
+    """Uses pydantic to define settings for the project."""
 
     alpha_api_key: str
     db_name: str
@@ -34,6 +39,5 @@ class Settings(BaseSettings):
 settings = Settings(
     alpha_api_key="1LDHCZV19GB3C9QF",
     db_name="miscrosoft_corporation_financial_data",
-    model_directory="Volatility-Forecasting-In-Microsoft-Corporation-By-Minute
-/2024-01-05T14:16:39.571936_MSFT.pkl",
+    model_directory=get_model_directory(),
 )
